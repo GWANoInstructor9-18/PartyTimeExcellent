1243b3c80938f097f874201da826b5166a956f27
GlasgowSmile04 <zacglasgow@gmail.com>
2018-10-03 20:21:17 -0700

    Changed list of parties to onload

--- js/scripts.js
+++ js/scripts.js
@@ -11,48 +11,7 @@ const partyList = document.getElementById('partyList');
 
 let parties;
 
-parties = [{
-  id: 1,
-  creator: 'Zac',
-  eventName: 'Halloween',
-  address: '700 Van Ness',
-  city: 'Fresno',
-  state: 'CA',
-  zip: '93721',
-  ageRestricted: true,
-  private: false,
-  date: '10/31/2018',
-  time: '7:00pm',
-  description: 'This is a generic party.'
-}, 
-{
-id: 2,
-creator: 'Phil',
-eventName: 'Kegger',
-address: '123 Test St.',
-city: 'Visalia',
-state: 'CA',
-zip: '93291',
-ageRestricted: false,
-private: true,
-date: '12/25/2018',
-time: '12:00pm',
-description: 'This is a generic christmas kegger.'
-},
-{
-id: 3,
-creator: 'John',
-eventName: 'Runescape LAN',
-address: '999 Johns house',
-city: 'Tulare',
-state: 'CA',
-zip: '93724',
-ageRestricted: true,
-private: true,
-date: '10/01/2018',
-time: '9:00am',
-description: 'This is an extra special LAN party.'
-}]
+
 
 createBtn.onclick = function() {
     modal.style.display = "block";
@@ -68,17 +27,19 @@ window.onclick = function(event) {
     }
 }
 
-findBtn.addEventListener('click', e => {
+window.onload = function(){
+    for(let i = 0; i <= parties.length; i++){
+        let eventName = parties[i].eventName;
+        let time = parties[i].time;
+        let date = parties[i].date;
+        let description = parties[i].description;
 
-    let eventName = parties[0].eventName;
-    let time = parties[0].time;
-    let date = parties[0].date;
-    let description = parties[0].description;
-
-    newParty(eventName, time, date, description);
+        newParty(eventName, time, date, description);
+    
+    }
     
 
-})
+}
 
 function newParty(eventName, time, date, description){
     let partyDiv = document.createElement('div');
@@ -90,3 +51,47 @@ function newParty(eventName, time, date, description){
     partyList.appendChild(partyLi);
     
 }
+
+
+parties = [{
+    id: 1,
+    creator: 'Zac',
+    eventName: 'Halloween',
+    address: '700 Van Ness',
+    city: 'Fresno',
+    state: 'CA',
+    zip: '93721',
+    ageRestricted: true,
+    private: false,
+    date: '10/31/2018',
+    time: '7:00pm',
+    description: 'This is a generic party.'
+  }, 
+  {
+  id: 2,
+  creator: 'Phil',
+  eventName: 'Kegger',
+  address: '123 Test St.',
+  city: 'Visalia',
+  state: 'CA',
+  zip: '93291',
+  ageRestricted: false,
+  private: true,
+  date: '12/25/2018',
+  time: '12:00pm',
+  description: 'This is a generic christmas kegger.'
+  },
+  {
+  id: 3,
+  creator: 'John',
+  eventName: 'Runescape LAN',
+  address: '999 Johns house',
+  city: 'Tulare',
+  state: 'CA',
+  zip: '93724',
+  ageRestricted: true,
+  private: true,
+  date: '10/01/2018',
+  time: '9:00am',
+  description: 'This is an extra special LAN party.'
+  }]
