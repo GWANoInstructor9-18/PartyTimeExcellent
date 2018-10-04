c10e0f54645760cf8e2e1e47db257e3f494c9d5e
matthewwyatt3 <you@example.com>
2018-10-03 19:07:13 -0700

    create button opens modal

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,24 @@ parties = [{
   description: 'This is a generic party.'
 }]
 
+
+//create modal functions
+var modal = document.getElementById('createModal');
+
+var btn = document.getElementById('createBtn');
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
