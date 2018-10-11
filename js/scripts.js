f8a5f8aa4110f40e0ff23dac28a126665a3d9b88
lulu-1216 <lulu07@mail.fresnostate.edu>
2018-10-10 19:45:36 -0700

    favicon added

--- js/scripts.js
+++ js/scripts.js
@@ -96,11 +96,7 @@ constructor(){
 
     checkNewParty(newParty);
 
-    //APPENDABLES
-    partyLi.append(eventName, time, date, description);
-    partyDiv.append(partyLi);
-    partyList.appendChild(partyLi);
-
+}
 }
 
 function checkAgeRadios(getAgeRadios, newParty){
@@ -146,61 +142,6 @@ function checkNewParty(newParty){
         createModal.style.display = 'none';
 }
 
-<<<<<<< HEAD
-parties = [{
-    id: 1,
-    creator: 'Zac',
-    eventName: 'Halloween',
-    address: '700 Van Ness',
-    city: 'Fresno',
-    state: 'CA',
-    zip: '93721',
-    ageRestricted: true,
-    private: false,
-    date: '10/31/2018',
-    time: '7:00pm',
-    description: 'This is a generic party.'
-  },
-  {
-  id: 2,
-  creator: 'Phil',
-  eventName: 'Kegger',
-  address: '123 Test St.',
-  city: 'Visalia',
-  state: 'CA',
-  zip: '93291',
-  ageRestricted: false,
-  private: true,
-  date: '12/25/2018',
-  time: '12:00pm',
-  description: 'This is a generic christmas kegger.'
-  },
-  {
-  id: 3,
-  creator: 'John',
-  eventName: 'Runescape LAN',
-  address: '999 Johns house',
-  city: 'Tulare',
-  state: 'CA',
-  zip: '93724',
-  ageRestricted: true,
-  private: true,
-  date: '10/01/2018',
-  time: '9:00am',
-  description: 'This is an extra special LAN party.'
-  }]
-
-  function clearCreateForm(){
-      getEventName.value = '';
-      getStreetAddress.value = '';
-      getCity.value = '';
-      getState.value = '';
-      getZip.value = '';
-      getDate.value = '';
-      getTime.value = '';
-      getDescription.value = '';
-  }
-=======
 function clearCreateForm(){
     getEventName.value = '';
     getStreetAddress.value = '';
@@ -211,4 +152,3 @@ function clearCreateForm(){
     getTime.value = '';
     getDescription.value = '';
 }
->>>>>>> 16c5cd2423b3aebd31c4d9867597aa8c85e384d5
